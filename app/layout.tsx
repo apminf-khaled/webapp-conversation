639c816640b6dc608c3b0650a39b71024c72dccb
apminf-khaled <135373672+apminf-khaled@users.noreply.github.com>
2024-07-12 14:58:10 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,31 @@
+import { getLocaleOnServer } from '@/i18n/server';
+
+import './styles/globals.css';
+import './styles/markdown.scss';
+
+const LocaleLayout = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  const locale = getLocaleOnServer();
+  return (
+    <html lang={locale ?? 'en'} className="h-full">
+      <body className="h-full">
+        <div className="overflow-x-auto">
+          <div className="w-screen h-screen min-w-[300px]">
+            {/* إضافة الصورة هنا */}
+            <img src="/1111.jpg" alt="Libyan International University Law" className="header-image" />
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
+
+export default LocaleLayout;
+
 import { getLocaleOnServer } from '@/i18n/server'
 
 import './styles/globals.css'
