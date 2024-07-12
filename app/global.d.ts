ade2e814085fbbfd43037f463720ffd9392a5e7f
apminf-khaled <135373672+apminf-khaled@users.noreply.github.com>
2024-07-12 14:59:55 +0300

    Update global.d.ts

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,2 +1,11 @@
-declare module 'dify-client';
-declare module 'uuid';
+declare module 'LIMU-client';
+declare module 'LIMU';
+.header-image {
+  width: 100%;
+  height: auto;
+  max-width: 600px; /* تحديد الحد الأقصى لعرض الصورة */
+  margin-bottom: 20px; /* إضافة مسافة تحت الصورة */
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
